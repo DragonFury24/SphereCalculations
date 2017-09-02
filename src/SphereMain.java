bbb095cb97f2ac976c2106eb1bcb9057f5be9b13
Duy <dennisdnguyen19@gmail.com>
2017-09-01 22:01:51 -0400

    Initial Commit

---
+++ src/SphereMain.java
@@ -0,0 +1,35 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SphereMain {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please input the radius of the sphere you are trying to solve.");
+        double radius = doubleInput(keyboard);
+        System.out.println("Diameter: " + radius * 2);
+        System.out.println("Circumference: " + 2 * Math.PI * radius);
+        System.out.println("Surface Area: " + 4 * Math.PI * Math.pow(radius, 2));
+        System.out.println("Volume: " + (4 / 3) * Math.PI * Math.pow(radius, 3));
+    }
+
+    public static double doubleInput(Scanner keyboard) { //checks if input is a double. If not, then will give error and force user to retry
+        double dInput = -1;
+
+        while (dInput == -1) {
+            try {
+                dInput = keyboard.nextDouble();
+
+                if (dInput < 0) {
+                    dInput = -1;
+                    System.out.println("You put in a number less than 0. Please try again.");
+                }
+            } catch (InputMismatchException I) {
+                System.out.println("You did not input a number. Please try again.");
+                keyboard.next();
+            }
+        }
+
+        return dInput;
+    }
+}
